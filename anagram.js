f406391a8b4e974d7cd610e2408d11d454539793
OksanaSam <60159629+OksanaSam@users.noreply.github.com>
2020-09-21 23:06:45 -0400

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,39 @@
+Check if two strings are anagrams
+
+///////////////////////////////////////////////////
+
+     const checkforAnagram = (strOne, strTwo) => {
+        if (strOne.length !== strTwo.length) {
+          console.log("false");
+          return false;
+        }
+        
+        const createHashObject = (str) => {
+          let obj = {};
+          str.split("").map((element) => {
+            let counter = 1;
+            if (Object.keys(obj).includes(element)) {
+              obj[element] = counter + 1;
+            } else {
+              obj[element] = counter;
+            }
+          });
+          return obj;
+        };
+
+        let hashObj1 = createHashObject(strOne);
+        let hashObj2 = createHashObject(strTwo);
+
+        // console.log("HashObjectOne:", hashObj1, "HashObjectTwo:", hashObj2);
+
+        for (let property in hashObj1) {
+          if (hashObj1[property] !== hashObj2[property]) {
+            console.log("false");
+            return false;
+          }
+        }
+        console.log("true");
+        return true;
+      };
+
+      checkforAnagram("jelhhdlo", "ellhhdjo");
